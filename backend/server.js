71bf56247236ce8b85f9f607378f25fb8d3beca6
Ansh Agarwal <anshagrawal12348@gmail.com>
2025-05-30 11:20:34 +0530

    Add Redis client configuration and connection logic

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const UserRoutes = require('./routes/user');
 const CustomerRoutes = require('./routes/customer');
 const CampaignRoutes = require('./routes/campaign');
 const VendorRoutes = require('./routes/vendor');
+const { connectRedis } = require('./config/redisClient');
 
 
 const port = 3000;
@@ -26,8 +27,19 @@ app.use('/api/vendor', VendorRoutes);
 
 // connect to the database
 
+
+(async () => {
+    try {
+        await connectRedis();
+    } catch (error) {
+        console.error('Error connecting to Redis:', error);
+    }
+}
+)();
+
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("connected to the database");
+
 }).catch((error) => {
     console.log("error ", error);
 });

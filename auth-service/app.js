d92ef9cf197398994bd5e4187cbbeb0ffe6681e0
Ansh Agarwal <anshagrawal12348@gmail.com>
2025-07-08 18:24:12 +0530

    feat(auth-service): initialize auth service with user routes and dependencies

---
+++ auth-service/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const UserRoutes = require('./routes/user');
+
+const port = process.env.PORT || 3000;
+
+app.use(cors({ origin: '*'}));
+app.use(express.json());
+app.use('/api/user', UserRoutes);
+
+
+
+// connect to the database
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
+})();
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});

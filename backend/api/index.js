d42ac2e3bc871247cf660ac5ca4e63c41897a18b
Ansh Agarwal <anshagrawal12348@gmail.com>
2025-07-08 17:21:16 +0530

    Increase request body size limits for JSON and URL-encoded data

--- backend/api/index.js
+++ backend/api/index.js
@@ -16,7 +16,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: '*'}));
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 
 app.use('/api/user', UserRoutes);
 app.use('/api/customer', CustomerRoutes);
